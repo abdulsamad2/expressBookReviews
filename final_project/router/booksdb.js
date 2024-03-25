d09482be34d6983e8f957c2bbeac07c358472b67
AbdulSamad <abdulsamad.laghari1@gmail.com>
2024-03-25 15:24:31 +0500

    complete general routes

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,77 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+  1: {
+    isbn: "9780385474542",
+    author: "Chinua Achebe",
+    title: "Things Fall Apart",
+    reviews: [
+      { user: "User1", rating: 4, comment: "Great book!" },
+      { user: "User2", rating: 5, comment: "A must-read!" },
+    ],
+  },
+  2: {
+    isbn: "9781435162690",
+    author: "Hans Christian Andersen",
+    title: "Fairy tales",
+    reviews: [
+      { user: "User3", rating: 3, comment: "Nice collection of stories." },
+    ],
+  },
+  3: {
+    isbn: "9780812967218",
+    author: "Dante Alighieri",
+    title: "The Divine Comedy",
+    reviews: [
+      {
+        user: "User4",
+        rating: 5,
+        comment: "One of the greatest works of literature.",
+      },
+    ],
+  },
+  4: {
+    isbn: "9780140441000",
+    author: "Unknown",
+    title: "The Epic Of Gilgamesh",
+    reviews: [{ user: "User5", rating: 4, comment: "Fascinating epic!" }],
+  },
+  5: {
+    isbn: "9780140441048",
+    author: "Unknown",
+    title: "The Book Of Job",
+    reviews: [
+      { user: "User6", rating: 5, comment: "Deep and thought-provoking." },
+    ],
+  },
+  6: {
+    isbn: "9780140449389",
+    author: "Unknown",
+    title: "One Thousand and One Nights",
+    reviews: [{ user: "User7", rating: 4, comment: "Classic tales!" }],
+  },
+  7: {
+    isbn: "9780140445305",
+    author: "Unknown",
+    title: "Njál's Saga",
+    reviews: [{ user: "User8", rating: 3, comment: "Interesting saga." }],
+  },
+  8: {
+    isbn: "9780141439518",
+    author: "Jane Austen",
+    title: "Pride and Prejudice",
+    reviews: [{ user: "User9", rating: 5, comment: "Love this book!" }],
+  },
+  9: {
+    isbn: "9780486427034",
+    author: "Honoré de Balzac",
+    title: "Le Père Goriot",
+    reviews: [{ user: "User10", rating: 4, comment: "Powerful story." }],
+  },
+  10: {
+    isbn: "9780802144396",
+    author: "Samuel Beckett",
+    title: "Molloy, Malone Dies, The Unnamable, the trilogy",
+    reviews: [{ user: "User11", rating: 4, comment: "Thought-provoking!" }],
+  },
+};
 
-module.exports=books;
+module.exports = books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,69 @@
-const express = require('express');
+const express = require("express");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get("/", function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  if (!books)
+    return res.status(500).json({ message: "There is no book in databse" });
+  return res.status(201).json(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get("/isbn/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+  const isbn = req.params.isbn;
+  if (!isbn) return res.status(400).json({ message: "ISBN is required" });
+
+  //books is object i need to filter it
+  const filteredBook = Object.keys(books).filter(
+    (key) => books[key].isbn === isbn
+  );
+  return res.status(201).json({ book: books[filteredBook] });
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get("/author/:author", function (req, res) {
+  const author = req.params.author;
+  if (!author) return res.status(400).json({ message: "Author is required" });
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const authorBooks = Object.values(books).filter(
+    (book) => book.author === author
+  );
+  return res.status(201).json({ books: authorBooks });
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get("/title/:title", function (req, res) {
+  const title = req.params.title;
+  if (!title) return res.status(400).json({ message: "title is required" });
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const titleBooks = Object.values(books).filter(
+    (book) => book.title === title
+  );
+  return res.status(201).json({ books: titleBooks });
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get("/review/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (!isbn) return res.status(400).json({ message: "ISBN is required" });
+
+  //books is object i need to filter it
+  const filteredBook = Object.keys(books).filter(
+    (key) => books[key].isbn === isbn
+  );
+  console.log(filteredBook);
+  return res.status(201).json({ review: books[filteredBook].reviews });
 });
 
 module.exports.general = public_users;
